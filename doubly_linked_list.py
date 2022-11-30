77128bb4bba0d41b5ee5c58fe2c10974f1ad8212
Bruna Maria <bruna.mariap@outlook.com>
2022-11-29 22:06:40 -0300

    arquivos do projeto

---
+++ doubly_linked_list.py
@@ -0,0 +1,108 @@
+class Node:
+
+    def __init__(self, data, prev_node, next_node):
+        self.data = data
+        self.prev_node = prev_node
+        self.next_node = next_node
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        #são 2 referencias que nao podem sair
+        self.header = Node(None, None, None)
+        self.trailer = Node(None, None, None)
+        
+        self.header.next_node = self.trailer
+        self.trailer.prev_node = self.header
+        
+        self.size = 0
+
+    def is_empty(self):
+        """ Checa se a lista está vazia """
+        return self.size == 0
+
+    def __len__(self):
+        return self.size
+
+    def print_list(self):
+        cur_node = self.header.next_node
+        """ print('\nElementos da lista:', end=' ') """
+        while cur_node.next_node != None:
+            print(cur_node.data, end='')
+            cur_node = cur_node.next_node
+    
+    def insert_between(self, item, predecessor, sucessor):
+        new_node = Node(item, predecessor, sucessor)
+        #muda os ponteiros
+        predecessor.next_node = new_node
+        sucessor.prev_node = new_node
+        self.size += 1
+
+    def insert_first(self, data):
+        # Nó deve entrar entre headerer e headerer.next
+        self.insert_between(data, self.header, self.header.next_node)
+
+    def insert_last(self, data):
+        self.insert_between(data, self.trailer.prev_node, self.trailer)
+
+    def delete_node(self, node): #passar nó que quero apagar
+        # Referência para o antecessor e o sucessor do nó que quero apagar
+        predecessor = node.prev_node 
+        sucessor = node.next_node
+
+        # Remove o nó pois os ponteiros para ele não existem mais
+        predecessor.next_node = sucessor
+        sucessor.prev_node = predecessor
+
+        self.size -= 1
+
+    def delete_last(self):
+        if self.is_empty():
+            print('Lista vazia!')
+        return self.delete_node(self.trailer.prev_node) #passo o último nó para ser deletado
+
+    def delete_first(self):
+        if self.is_empty():
+            print('Lista vazia!')
+        return self.delete_node(self.header.next_node) #passo o primeiro nó para ser deletado
+
+    def search(self, data):
+        cur_node = self.header
+        while cur_node.data != data:
+            print(f'\nO dado: {data} foi encontrado')
+            return 
+        cur_node = cur_node.next_node
+
+    def clear(self):
+        self.header.next_node = None
+        self.trailer.prev_node = None
+        
+        self.header.next_node = self.trailer
+        self.trailer.prev_node = self.header
+        
+        self.size = 0
+    
+    def mover(self, tecla):
+        key_press = tecla
+        if key_press == 'right':
+            if self.current_node.next_node is None: # Chegou no final
+                self.current_node = self.trailer.prev_node 
+            else:
+                self.current_node = self.current_node.next_node
+        elif key_press == 'left':
+            if self.current_node.prev_node is None: # Chegou no início
+                self.current_node = self.header.next_node
+            else:
+                self.current_node = self.current_node.prev_node
+
+    def insert(self, dado):
+        """ função que deve chamar insert_between para inserir um número em qualquer lugar """
+        if self.is_empty():
+            self.current_node = self.trailer.prev_node
+
+        self.insert_between(dado, self.current_node, self.current_node.next_node)
+        self.current_node = self.current_node.next_node
+
+    def delete(self):
+        """ Função que deve deletar qualquer coisa chamando a função delete_node"""
+        self.delete_node(self.current_node)
---
+++ interface_editor.py
@@ -0,0 +1,66 @@
+import keyboard
+from tkinter import *
+from doubly_linked_list import DoublyLinkedList
+
+master = Tk()
+master.title('Editor de texto')
+master.geometry('954x650+317+34')
+master.wm_resizable() #não permitir que a tela possa ser redimensionada
+
+def limpar():
+    text_description.delete('1.0', END)
+    lista.clear()
+
+# criação de frames
+frame_description = Frame(master, bg="#85B777")
+frame_description.place(width=801, height=579, x=12, y=21)
+
+#criação da barra lateral, scroll
+barra_lateral1 = Scrollbar(frame_description) #subir e descer o texto
+barra_lateral1.pack(side=RIGHT, fill=Y)
+
+# criação das caixas de texto
+text_description = Text(frame_description, font=('Calibri', 16), selectbackground='#F5F799', selectforeground='black', wrap=WORD, undo=True, yscrollcommand=barra_lateral1.set)
+text_description.place(width=784, height=579, x=0, y=0)
+
+#configurando a barra lateral
+barra_lateral1.config(command=text_description.yview) #associa a barra lateral ao texto que ela representa, a função faz a barra aumentar enquanto o texto diminui e diminuir se o texto aumenta
+
+# Criação dos botões
+bt2 = Button(master, text="Limpar", font=("Calibri", 16), background='white', command=limpar)
+bt2.place(width=80, height=52, x=834, y=123)
+
+lista = DoublyLinkedList()
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzç'
+keyboard.block_key('up')
+keyboard.block_key('down')
+def press_backspace(event):
+    lista.delete_last()
+    """ print()
+    lista.print_list() """
+
+def press_right(event):
+    lista.mover('right')
+    """ print()
+    lista.print_list() """
+
+def press_left(event):
+    lista.mover('left')
+    """ print()
+    lista.print_list() """
+
+def key_press(event):
+    key = event.char
+    if key in alphabet or key in alphabet.upper() or ' ':
+        lista.insert(key)
+        """ print()
+    lista.print_list() """
+
+master.bind('<Key>', key_press) # Vincula uma função a uma tecla pressionada
+master.bind("<BackSpace>", press_backspace)
+master.bind("<Right>", press_right)
+master.bind("<Left>", press_left)
+
+master.mainloop()
+lista.print_list()
